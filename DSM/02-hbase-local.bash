7d3c0c29b5eb8d4b0686e291249d5b191f41d0a2
Frank S <frank.slyne@gmail.com>
2015-10-28 00:17:38 +0000

    Create 02-hbase-local.bash

---
+++ DSM/02-hbase-local.bash
@@ -0,0 +1,33 @@
+IAM=$(id -nu)
+HDUSER='hduser'
+if [ "$IAM" != "$HDUSER" ];
+then
+   echo "You need to be logged in as $HDUSER"
+   exit
+else
+   echo "Correct user"
+fi
+
+
+cd /usr/local/hbase
+bin/stop-hbase.sh
+
+cd /usr/local/hadoop
+sbin/stop-yarn.sh
+sbin/stop-dfs.sh
+
+cd /usr/local/hadoop/etc/hadoop
+cp hdfs-site.xml.local hdfs-site.xml
+cp core-site.xml.local core-site.xml
+
+cd /usr/local/hbase/conf
+cp hbase-site.xml.local hbase-site.xml
+
+cd /usr/local/hadoop
+sbin/start-dfs.sh
+sbin/start-yarn.sh
+
+cd /usr/local/hbase/
+bin/start-hbase.sh
+
+jps
